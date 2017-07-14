c54b7817ed7517395d9d0dfd637bf13ecfbe4824
Antonio Salvati <antonio.salvati@skillbill.it>
2017-07-14 10:54:16 +0200

    yt integration

---
+++ public/js/yt.js
@@ -0,0 +1,39 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '670742227434-uc7h98jd4fe571tfi2pjtuvv24spd058.apps.googleusercontent.com';
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+// Authorization scopes required by the API. If using multiple scopes,
+// separated them with spaces.
+var SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+var uploadButton = document.querySelector('button.upload');
+var uploadForm = document.querySelector('.uploadForm');
+uploadButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  uploadVideo();
+});
+
+function uploadVideo() {
+
+  var initClient = function() {
+    gapi.client.init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+    gapi.auth2.getAuthInstance().signIn()
+  }
+  
+  gapi.load('client:auth2', initClient);
+  
+  function updateSigninStatus() {
+    uploadForm.classList.remove("hidden");
+  }
+  
+}
+
