e7bc7702fb7845bfa9471258474a9898e500c81c
Enrico Bardelli <enrico.bardelli@skillbill.it>
2017-07-14 11:48:44 +0200

    vote

--- routers/root.js
+++ routers/root.js
@@ -10,9 +10,17 @@ router.get('/date', function(req, res) {
   });
 });
 
+const paged = (pageNumber, sortFunc) => {
+  return database.getAllVideo().sort(sortFunc).slice(pageNumber*pageSize, (pageNumber+1)*pageSize);
+};
+
 router.post('/upload', function(req, res) {
   const {videoId} = req.body;
   const oid = req.user.oid;
+
+  if(database.getVideo(videoId)) {
+    return res.status(409).send(`video already exist with id ${videoId}`);
+  }
 
   const video = {
     videoId,
@@ -30,13 +38,28 @@ router.post('/upload', function(req, res) {
 });
 
 router.get('/list-new', function(req, res) {
-  const pageNumber = req.query.pageNumber;
-
-  const page = database.getAllVideo().sort((video1, video2) => {
+  res.status(200).send(paged(req.query.pageNumber, (video1, video2) => {
     return video1.uploadDate < video2.uploadDate;
-  }).slice(pageNumber*pageSize, (pageNumber+1)*pageSize);
+  }));
+});
 
-  res.status(200).send(page);
+router.put('/vote', function(req, res) {
+  const {videoId, like, love, happy, dislike} = req.body;
+  const video = database.getVideo(videoId);
+  if(!video) {
+    return res.status(404).send('cannot find video');
+  }
+
+  video.like += like ? 1 : 0;
+  video.love += love ? 1 : 0;
+  video.happy += happy ? 1 : 0;
+  video.dislike += dislike ? 1 : 0;
+
+  if(database.updateVideo(video)) {
+    res.status(200).send(video);
+  } else {
+    res.status(404).send('cannot update video');
+  }
 });
 
 module.exports = router;
