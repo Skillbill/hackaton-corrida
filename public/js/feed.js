631109fb11bea10ba1cb8d6c96604254c5e045df
Enrico Bardelli <enrico.bardelli@skillbill.it>
2017-07-14 14:35:31 +0200

    vote

--- public/js/feed.js
+++ public/js/feed.js
@@ -11,17 +11,41 @@ const feed = {
       })
       .then((response) => {
           return response.json();
-        }).then(function(j) {
-          j.forEach(video => {
+        }).then(function(json) {
+          json.forEach(video => {
             const templateStr = document.getElementById('template-video').innerHTML;
             const newVideo = Mustache.render(templateStr, video);
             const elem = document.createElement('div');
+            elem.setAttribute('id',video.videoId);
             elem.classList.add('video-card');
             elem.innerHTML = newVideo;
             document.getElementById('feed').appendChild(elem);
           });
 
         });
+    });
+  },
+
+  vote: (videoId, voteType) => {
+    console.log(videoId, voteType);
+    user.getIdToken().then((auth_token) => {
+      fetch('/api/vote', {
+        method: 'PUT',
+        headers: {'Authorization': 'Bearer ' + auth_token, 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          "videoId": videoId,
+          "like": voteType=='like',
+          "love": voteType=='love',
+          "happy": voteType=='happy',
+          "dislike": voteType=='dislike'
+        })
+      }).then((response) => {
+        return response.json();
+      }).then(function(video) {
+        const templateStr = document.getElementById('template-video').innerHTML;
+        const newVideo = Mustache.render(templateStr, video);
+        document.getElementById(video.videoId).innerHTML = newVideo;
+      })
     });
   }
 
